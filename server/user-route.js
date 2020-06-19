cac9def737070165dcd92d49c4608208a38a31a8
Ben Bodine <benjaminbodine@Bens-MacBook-Pro-6.local>
2020-06-19 14:03:53 -0400

    Added features to user post request (for creating new account) stores data in db, pword is salted & hashed, returns 400 error if user with same email is already in db

--- server/user-route.js
+++ server/user-route.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const User = require('./Users.js');
 
 
 /* Post a new user to db - for account creation*/
 router.post('/', async (req, res) => {
-    console.log('user post request');
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password,
-        admin: req.body.admin
-    })
+    
     try {
+        const same_email = User.find( {"email": req.body.email });
+        if(same_email != null) {
+            res.status(400).send('A user with this email already exists.');
+            return;
+        }
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = new User({
+            email: req.body.email,
+            password: hashedPassword,
+            admin: req.body.admin
+        })
         const savedUser = await user.save();
         res.json(savedUser);
     } catch(err){
