b5850544919369d50409523cc956560f00d49186
Ben Bodine <benjaminbodine@Bens-MacBook-Pro-6.local>
2020-06-22 11:48:42 -0400

    Finished basic backend functionality for creating user accounts and authenticating users on login

--- server/user-route.js
+++ server/user-route.js
@@ -6,10 +6,9 @@ const User = require('./Users.js');
 
 /* Post a new user to db - for account creation*/
 router.post('/', async (req, res) => {
-    
-    try {
-        const same_email = User.find( {"email": req.body.email });
-        if(same_email != null) {
+    try { //Verify an email is valid? if thats possible
+        const same_email = await User.findOne( {"email": req.body.email }, {"email": 1}); 
+        if(await same_email != null) { 
             res.status(400).send('A user with this email already exists.');
             return;
         }
@@ -22,6 +21,24 @@ router.post('/', async (req, res) => {
         const savedUser = await user.save();
         res.json(savedUser);
     } catch(err){
+        res.json({message: err});
+    }
+});
+
+/*Post request to authenticate user */
+router.post('/login', async (req, res) => {
+    try {
+        const pword = await User.findOne( {"email": req.body.email}, {"password": 1} );
+        if(pword === null) {
+            res.status(404).send('There is no user with this email.');
+            return;
+        }
+        if(await bcrypt.compare(req.body.password, pword.password)) {
+            res.send('Successful login');
+        } else {
+            res.status(403).send('User name and password do not match.');
+        }
+    } catch(err) {
         res.json({message: err});
     }
 });
