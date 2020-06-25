293f7e4082e0fb2d02bb44829bc07b8541a3969d
Ben Bodine <benjaminbodine@Bens-MacBook-Pro-6.local>
2020-06-25 12:00:26 -0400

    moving my code into setup jasper made

---
+++ server/api/models/Orgs.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const OrgSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    employees: {
+        type: Array,
+        default: []
+    },
+
+    admins: {
+        type: Array,
+        default: []
+    }
+});
+
+module.exports = mongoose.model('Orgs', OrgSchema);
---
+++ server/api/routes/org.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const Joi = require('@hapi/joi');
+const Org = require('./Orgs.js');
+
+/* All these will need to be restricted to just admin user */
+
+router.post('/admins', async (req, res) => {
+    try {
+        const org = await Org.findOne( {"name": req.body.org });
+        const alreadyAdded = org.admins.find(a => a === req.body.new_admin);
+        if(alreadyAdded != undefined) {
+            res.status(400).send('This email has already been added as an admin for the organization.');
+            return;
+        }
+        await Org.updateOne({_id: org._id}, {$push: {admins: req.body.new_admin}});
+        res.json(org.admins);
+    } catch (err){
+        res.json({message: "Failed to add admin to org"});
+    }
+});
+
+router.post('/employees', async (req, res) => { 
+    try {
+        const org = await Org.findOne( {"name": req.body.org });
+        const alreadyAdded = org.employees.find(a => a === req.body.employee);
+        if(alreadyAdded != undefined) {
+            res.status(400).send('This email has already been added as an employee for the organization.');
+            return;
+        }
+        await Org.updateOne({_id: org._id}, {$push: {employees: req.body.employee}});
+        res.json(org.employees);
+    } catch (err){
+        res.json({message: "Failed to add employee to org"});
+    }
+});
+
+//Returns list of admins
+router.get('/admins/:org', async (req, res) => {
+    try {
+        console.log('bruh'); 
+        const org = await Org.findOne( {"name": req.params.org} );
+        console.log(org.admins);
+        res.json(org.admins);
+    } catch(err) {
+        res.json({message: ""})
+    }
+});
+
+module.exports = router; 
