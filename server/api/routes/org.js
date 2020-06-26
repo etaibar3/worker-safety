c3aa11a28f77fa3d03e6e6fd1229f14295f970f0
Ben Bodine <benjaminbodine@Bens-MacBook-Pro-6.local>
2020-06-26 12:42:39 -0400

    Fixed manageRoster post to add user to org, and implemented delete to remove employee from org

--- server/api/routes/org.js
+++ server/api/routes/org.js
@@ -5,35 +5,62 @@ const Org = require('../models/Orgs.js');
 
 /* All these will need to be restricted to just admin user */
 
-router.post('/admins', async (req, res) => {
+const userValidation = Joi.object ({
+    email: Joi.string().email().required(),    
+    admin: Joi.bool().required(),
+    org: Joi.string().required()
+});
+
+//Adds an admin to an organization so they can create an account
+router.post('/manageRoster', async (req, res) => {
+    try {//Data Validation
+        const { error } = await userValidation.validateAsync(req.body);
+    } catch (error){ 
+        return res.status(400).send(error.details[0].message);
+    }
     try {
         const org = await Org.findOne( {"name": req.body.org });
-        const alreadyAdded = org.admins.find(a => a === req.body.new_admin);
-        if(alreadyAdded != undefined) {
-            res.status(400).send('This email has already been added as an admin for the organization.');
+
+        const alreadyAdmin = await org.admins.find(a => a === req.body.email);
+        const alreadyEmployee = await org.employees.find(e => e === req.body.email);
+
+        if(alreadyAdmin != undefined || alreadyEmployee != undefined) {
+            res.status(400).send('This email has already been added to the organization.');
             return;
         }
-        await Org.updateOne({_id: org._id}, {$push: {admins: req.body.new_admin}});
-        res.json(org.admins);
+        if(await req.body.admin === "true")  {
+            await Org.updateOne({_id: org._id}, {$push: {admins: req.body.email}});
+            res.json(org.admins);
+        }
+         else {
+           await Org.updateOne({_id: org._id}, {$push: {employees: req.body.email}});
+           res.json(org.employees);
+        }
     } catch (err){
-        res.json({message: "Failed to add admin to org"});
+        res.status(400).json({message: "Failed to add user to org"});
     }
 });
 
-router.post('/employees', async (req, res) => { 
-    try {
+router.delete('/manageRoster', async (req, res) => {
+    try{
         const org = await Org.findOne( {"name": req.body.org });
-        const alreadyAdded = org.employees.find(a => a === req.body.employee);
-        if(alreadyAdded != undefined) {
-            res.status(400).send('This email has already been added as an employee for the organization.');
-            return;
+        const anAdmin = await org.admins.find(a => a === req.body.email);
+        const anEmployee = await org.employees.find(e => e === req.body.email);
+        if(anAdmin != undefined) {
+            await Org.updateOne({name: req.body.org}, {$pull: {admins: req.body.email}})
+            res.send(anAdmin);
+        } else if (anEmployee != undefined) {
+            await Org.updateOne({name: req.body.org}, {$pull: {employees: req.body.email}});
+            res.send(anEmployee);
+        } else {
+            res.status(400).send('User not found: could not delete.');
+            return
         }
-        await Org.updateOne({_id: org._id}, {$push: {employees: req.body.employee}});
-        res.json(org.employees);
-    } catch (err){
-        res.json({message: "Failed to add employee to org"});
+        
+    } catch (err) {
+        res.status(400).json({message: "Failed to remove user from org"});
     }
-});
+})
 
 //Returns list of admins
 router.get('/admins/:org', async (req, res) => {
