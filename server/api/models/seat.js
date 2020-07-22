34c4ffd8e2c0d23a784b4d3325211648ffab937b
Etai <57533612+etaibar3@users.noreply.github.com>
2020-07-22 11:32:53 -0400

    Update seat.js

--- server/api/models/seat.js
+++ server/api/models/seat.js
@@ -24,7 +24,7 @@ const SeatSchema = new Schema({
     type: Boolean,
     default: true,
   },
-  $geometry: GeoSchema,
+  geometry: GeoSchema,
 });
 
 module.exports = mongoose.model("Seat", SeatSchema);
