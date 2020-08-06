0c8b54e754730d0d5aded3a44d792ab2b3ed5d02
Ben Bodine <benjaminbodine@Bens-MacBook-Pro-6.local>
2020-08-06 17:16:20 -0400

    gets people who were in office same day as sick user

--- server/api/routes/report.js
+++ server/api/routes/report.js
@@ -32,26 +32,42 @@ router.post('/', authenticateUser, async function(req, res){
     var report_date = req.body.date;
       // Use of Date.now() method 
     try{
+        const user = await User.findById(person_name);
         const today = new Date(Date.now());
+        //const today = new Date("2020-08-20")
         const reported = new Date(report_date); //this gets 8pm the day before for some reason
-        //console.log(report_date);
         //const result1 = await txc.run( 'Match (a:Users {m_id: $Name}) SET a.COVID19 = true, a.date = date($date) Return a', {Name: person_name, date: report_date})
-        const result1 = await txc.run( 'Match (a:Users {m_id: $Name}) -[:Reserved] -(b:Seat) - [:Within_dist] - (c:Seat)-[:Reserved] - (d:Users) return d', {Name: person_name})
-        const result2 = await txc.run( 'Match (a:Users {m_id: $Name}) -[:Reserved] -(b:Seat) - [:Within_dist] - (c:Seat)-[r:Reserved] - (d:Users) return r', {Name: person_name})
+        //const result1 = await txc.run( 'Match (a:Users {m_id: $Name}) -[:Reserved] -(b:Seat) - [:Within_dist] - (c:Seat)-[:Reserved] - (d:Users) return d', {Name: person_name})
+        //const result2 = await txc.run( 'Match (a:Users {m_id: $Name}) -[:Reserved] -(b:Seat) - [:Within_dist] - (c:Seat)-[r:Reserved] - (d:Users) return r', {Name: person_name})
+        /* Gets reservations made in org*/
+        const result3 = await txc.run( 'Match (a:Users) -[r:Reserved] -(b:Seat {org: $org}) return r', {org: req.user.org})
+        /* Gets who made the reservations*/
+        const result4 = await txc.run( 'Match (a:Users) -[r:Reserved] -(b:Seat {org: $org}) return a', {org: req.user.org})
+        /* Gets the reservations the sick user made*/
+        const result5 = await txc.run( 'Match (a:Users {m_id: $Name}) -[r:Reserved] -(b:Seat) return r', {Name: person_name})
         //const result3 = await txc.run( 'Match (a:Users {m_id: $Name}) -[:Reserved] -(b:Seat) -[:Reserved] - (d:Users) return d ', {Name: person_name})
         //const result4 = await txc.run('Match (n:Users {m_id: $Name}) return n', {Name: person_name})
         var exposed_persons = [];
         var index = 0;
-        for (const record of result1.records) {
-          const reserve_date = await new Date(result2.records[index]._fields[0].properties.time);
-          if(reserve_date <= today && reserve_date >= reported) {
-            const { email } = await User.findById(record._fields[0].properties.m_id)
-            const alreadyAdded = await exposed_persons.includes(email);
-            if(!alreadyAdded) {
+      
+        /* Gets the dates covid person was in office*/
+        const covid_dates = result5.records.map(record => {
+           const date = new Date(record._fields[0].properties.time)
+           return date.getTime()
+        })
+
+        /* Gets all people who made reservation same day as covid person*/
+        for (const record of result3.records) {
+          const reserve_date = await new Date(record._fields[0].properties.time)
+          const exposure = covid_dates.includes(reserve_date.getTime())
+          if(exposure && (reserve_date >= reported) && (reserve_date <= today)) {
+              const { email } = await User.findById(result4.records[index]._fields[0].properties.m_id)
+              const alreadyAdded =  exposed_persons.includes(email);  
+             if(!alreadyAdded && (user.email !== email)) {
               exposed_persons.push(email);
-            }
+             }
           }
-          index = index + 1;
+          index = index+ + 1
         }
         await txc.commit()
         console.log('committed')
