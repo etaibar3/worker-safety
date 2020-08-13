d5b5270f36e2f87b43b26cf506413c87e5aa72a7
jasper <jasperzhou1129@gmail.com>
2020-08-13 18:52:07 -0400

    renaming images

--- server/api/models/floorplan.js
+++ server/api/models/floorplan.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+const { int } = require("neo4j-driver");
 const floorplanSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, required: true },
+  name: { type: String },
   floorplanImage: { type: String, required: true },
-  org: { type: String, required: true }
+  org: { type: String, required: true },
+  floorNumber: { type: String, required: false },
 });
 
 module.exports = mongoose.model("Floorplan", floorplanSchema);
