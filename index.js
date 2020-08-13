771a0318bc144585687aac89d2e04fa2c39ffc01
Erin Yilmaz <yilmaz.erin@gmail.com>
2020-08-13 12:03:24 -0700

    added app express to top of index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
-const app = require("./app");
+const app = express();
+//const app = require("./app");
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const mongoose = require("mongoose");
