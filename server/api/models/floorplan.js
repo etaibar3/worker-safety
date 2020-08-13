854b381e2c32739996ee81f4b69c3678878da6b7
jasper <jasperzhou1129@gmail.com>
2020-08-13 18:30:34 -0400

    rename file

--- server/api/models/floorplan.js
+++ server/api/models/floorplan.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+const { int } = require("neo4j-driver");
 const floorplanSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, required: true },
+  name: { type: String },
   floorplanImage: { type: String, required: true },
+  org: { type: String, required: false },
+  floorNumber: { type: String, required: false },
 });
 
 module.exports = mongoose.model("Floorplan", floorplanSchema);
--- server/api/routes/floorplan.js
+++ server/api/routes/floorplan.js
@@ -6,6 +6,7 @@ const multer = require("multer");
 const fs = require("fs-extra");
 //const upload = multer({ dest: "uploads/" });
 const Floorplan = require("../models/floorplan");
+const { db } = require("../models/floorplan");
 var directory = path.join(__dirname + "/uploads");
 
 const fileFilter = (req, file, cb) => {
@@ -37,77 +38,63 @@ const upload = multer({
   fileFilter: fileFilter,
 });
 
-// const upload = multer({
-//   storage: storage,
-//   limits: {
-//     fileSize: 1024 * 1024 * 5,
-//   },
-//   fileFilter: fileFilter,
-// });
-
-router.get("/", (req, res, next) => {
-  Floorplan.find()
-    .select("name _id floorplanImage")
-    .exec()
-    .then((docs) => {
-      const response = {
-        count: docs.length,
-        products: docs.map((doc) => {
-          return {
-            name: doc.name,
-            _id: doc._id,
-            floorplanImage: doc.floorplanImage,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/floorplan/" + doc._id,
-            },
-          };
-        }),
-      };
-      res.status(200).json(response);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
+router.get("/", async (req, res, next) => {
+  try {
+    const results = await Floorplan.find();
+    const response = {
+      count: results.length,
+      products: results.map((result) => {
+        return {
+          name: result.name,
+          _id: result._id,
+          floorplanImage: result.floorplanImage,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/floorplan/" + result._id,
+          },
+        };
+      }),
+    };
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
-router.post("/", upload.single("floorplanImage"), (req, res, next) => {
-  console.log(req.file);
+router.post("/", upload.single("floorplanImage"), async (req, res, next) => {
   const floorplan = new Floorplan({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     floorplanImage: req.file.path,
+    org: req.body.org,
+    floorNumber: req.body.floorNumber,
   });
-  floorplan
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.status(201).json({
-        message: "Image uploaded successfully",
-        createdFloorplan: {
-          name: result.name,
-          floorplanImage: result.floorplanImage,
-          _id: result._id,
-          request: {
-            type: "GET",
-            url: "http//localhost:3000/floorplan/" + result._id,
-          },
+  try {
+    const result = await floorplan.save();
+    fs.rename(
+      result.floorplanImage,
+      "public/uploads/" + req.body.org + "-" + req.body.floorNumber + ".jpeg"
+    );
+    res.status(201).json({
+      message: "Image uploaded successfully",
+      createdFloorplan: {
+        name: result.name,
+        floorplanImage: result.floorplanImage,
+        _id: result._id,
+        request: {
+          type: "GET",
+          url: "http//localhost:3000/floorplan/" + result._id,
         },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
+      },
     });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
 router.get("/:floorplanId", async (req, res, next) => {
   const id = req.params.floorplanId;
+
   try {
     const result = await Floorplan.findById(id);
     if (result) {
