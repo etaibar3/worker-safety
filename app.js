ce623f22315362cca8c8c9304c4180bc6bd8f398
Erin Yilmaz <yilmaz.erin@gmail.com>
2020-08-14 09:23:50 -0700

    made true bool

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With,Content-Type, Accept, Authorization, auth, Access-Control-Allow-Origin"
