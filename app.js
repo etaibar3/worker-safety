d3814027a0475249bc1efc913048f521573478e6
Erin Yilmaz <yilmaz.erin@gmail.com>
2020-08-14 09:13:24 -0700

    wildcard

--- app.js
+++ app.js
@@ -56,14 +56,14 @@ app.use("/public/uploads", express.static("public/uploads"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With,Content-Type, Accept, Authorization, auth"
   );
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,Get");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
     return res.status(200).json({});
   }
   next();
