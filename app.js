cbf9b7c49ca361e85659dad44f26b5c55f35dd4e
Erin Yilmaz <yilmaz.erin@gmail.com>
2020-08-14 09:17:37 -0700

    added accesscontrolalloworigin

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With,Content-Type, Accept, Authorization, auth"
+    "Origin, X-Requested-With,Content-Type, Accept, Authorization, auth, Access-Control-Allow-Origin"
   );
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
