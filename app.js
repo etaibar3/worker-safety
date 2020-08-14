c775e88865fa09b6eef62e4f08e7cf25f223213e
Erin Yilmaz <yilmaz.erin@gmail.com>
2020-08-14 10:04:12 -0700

    adding OPTIONS

--- app.js
+++ app.js
@@ -60,12 +60,13 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With,Content-Type, Accept, Authorization, auth, Access-Control-Allow-Origin"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, auth, Access-Control-Allow-Origin"
   );
-  if (req.method === "OPTIONS") {
-    res.setHeader("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
-    return res.status(200).json({});
-  }
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS,PUT,POST,PATCH,DELETE,GET");
+  // if (req.method === "OPTIONS") {
+  //   res.setHeader("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+  //   return res.status(200).json({});
+  // }
   next();
 });
 
