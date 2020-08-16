4072ceaf248be944c12ce116590b668a36a5697c
Erin Yilmaz <yilmaz.erin@gmail.com>
2020-08-16 09:16:58 -0700

    comment in 65 app.js

--- app.js
+++ app.js
@@ -56,17 +56,17 @@ app.use("/public/uploads", express.static("public/uploads"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://safereturn.herokuapp.com");
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization, auth, Access-Control-Allow-Origin"
   );
-  //res.setHeader("Access-Control-Allow-Methods", "OPTIONS,PUT,POST,PATCH,DELETE,GET");
-   if (req.method === "OPTIONS") {
-     res.setHeader("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
-     return res.status(200).json({});
-   }
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS,PUT,POST,PATCH,DELETE,GET");
+   // if (req.method === "OPTIONS") {
+   //   res.setHeader("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+   //   return res.status(200).json({});
+   // }
   next();
 });
 
